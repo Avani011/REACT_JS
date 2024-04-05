82e095b537fbc528da7df70ab4a5d7bd4e7b1da3
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-04-06 03:38:55 +0530

    React Components

---
+++ react-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "./Head";
+import List from "./List";
+
+function App(){
+    return(
+        <div>
+            <Head />
+            <List />
+        </div>
+    )
+}
+
+export default App;
