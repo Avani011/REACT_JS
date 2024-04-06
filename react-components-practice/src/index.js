fe643d1b6e8ee97a99c71d6f12ed57cb8c4ae40c
Avani011 <102690647+Avani011@users.noreply.github.com>
2024-04-07 00:04:47 +0530

    React Components Practice

---
+++ react-components-practice/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
